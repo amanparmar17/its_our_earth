ab593788bf983b69b6014c7165d94fe85820dbb8
amanparmar17 <aman.parmar17@gmail.com>
2018-08-06 01:39:55 +0530

    Addedfiles and changes required for deployment to heroku

--- bot/models.py
+++ bot/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class contact_us(models.Model):
     name = models.CharField(max_length=70)
     email=models.EmailField(default="aaa@bb.c")
-    phone=models.BigIntegerField(default=10)
+    phone=models.CharField(default='xxxxxxxxxx')
     message=models.CharField(max_length=500)
     date=models.DateField(auto_now=True)
     time=models.TimeField(auto_now=True)
